f7c8c5b0248e978f3fd36ab63a45a0e659d36766
ignacioandresr <ignacioandresreynado@gmail.com>
2025-06-27 20:30:53 -0400

    up

--- html/js/funciones.js
+++ html/js/funciones.js
@@ -9,3 +9,16 @@ function togglePassword() {
         btn.textContent = "👁️";
       }
     }
+
+function copiarCorreo() {
+    const correo = 'soporte@winrecycle.cl';
+    
+    // Usamos la API Clipboard moderna
+    navigator.clipboard.writeText(correo).then(() => {
+      // Opcional: mostrar un mensaje o feedback
+      alert('Correo copiado al portapapeles');
+    }).catch(err => {
+      // En caso de error
+      console.error('Error al copiar al portapapeles: ', err);
+    });
+  }
