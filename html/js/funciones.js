301ab7122fca6b39f32788b107fce4a6497ac207
ignacioandresr <ignacioandresreynado@gmail.com>
2025-06-30 15:10:44 -0400

    up

--- html/js/funciones.js
+++ html/js/funciones.js
@@ -13,12 +13,43 @@ function togglePassword() {
 function copiarCorreo() {
     const correo = 'soporte@winrecycle.cl';
     
-    // Usamos la API Clipboard moderna
     navigator.clipboard.writeText(correo).then(() => {
-      // Opcional: mostrar un mensaje o feedback
       alert('Correo copiado al portapapeles');
     }).catch(err => {
-      // En caso de error
       console.error('Error al copiar al portapapeles: ', err);
     });
   }
+
+  function checkEmail() {
+    const emailInput = document.getElementById("exampleFormControlInput1");
+    const alert = document.getElementById("emailAlert");
+
+    if (emailInput.value !== "" && !emailInput.value.includes("@")) {
+      alert.classList.remove("d-none");
+    } else {
+      alert.classList.add("d-none");
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const checkboxes = document.querySelectorAll(".reciclaje-checkbox");
+    const btnEnviar = document.getElementById("btnEnviar");
+
+    function toggleButton() {
+      const isAnyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+      
+      if (isAnyChecked) {
+        btnEnviar.classList.remove("disabled");
+        btnEnviar.removeAttribute("disabled");
+      } else {
+        btnEnviar.classList.add("disabled");
+        btnEnviar.setAttribute("disabled", true);
+      }
+    }
+
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener("change", toggleButton);
+    });
+
+    toggleButton();
+  });
